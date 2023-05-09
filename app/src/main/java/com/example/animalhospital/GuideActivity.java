e7f627911b0d9481eaec1bcc2ca5d5a0a907a383
shinsam <shin321gg@gmail.com>
2023-05-09 10:56:18 +0900

    Initial commit

---
+++ app/src/main/java/com/example/animalhospital/GuideActivity.java
@@ -0,0 +1,22 @@
+package com.example.animalhospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+
+
+    }
+
+
+}
